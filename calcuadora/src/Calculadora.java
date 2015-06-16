938ff4decded9225607a0e7495268a7b763335a1
ss <s@j.com>
2015-06-16 18:33:49 -0500

    esqueleto de la clase v1

---
+++ calcuadora/src/Calculadora.java
@@ -0,0 +1,16 @@
+public class Calculadora {
+    private float num1;
+    private float num2;
+    public float suma(float var1,float var2){
+        return 0;
+    }
+    public float resta(float var1,float var2){
+        return 0;
+    }
+    public float multiplicacion(float var1,float var2){
+        return 0;
+    }
+    public float division(float var1,float var2){
+        return 0;
+    }
+}
---
+++ calcuadora/src/calcuadora/Calcuadora.java
@@ -0,0 +1,6 @@
+package calcuadora;
+public class Calcuadora {
+    public static void main(String[] args) {
+        Calcuadora calcu= new Calcuadora();
+    }
+}
